732e76bab1f3e5981e61d58201acb88e5e4b1ee7
Ben Wencke <benwencke@gmail.com>
2018-12-18 11:51:42 -0500

    font size

--- src/configs/props.js
+++ src/configs/props.js
@@ -87,7 +87,7 @@ export default {
     family: 'tahoma, arial, "Hiragino Sans GB", 宋体, sans-serif'
   }],
   converts: {
-    unitExportFn: (value, type) => type === 'line-height' ? value : `${value/10}rem`,
+    unitExportFn: (value, type) => type === 'line-height' ? value : `${value/10}em`,
   },
   emojis: [
     '🤣', '🙌', '💚', '💛', '👏', '😉', '💯',
