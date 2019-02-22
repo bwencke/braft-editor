eedf5230db7e986200c021f254d67b8c4136f840
Ben Wencke <benwencke@gmail.com>
2019-02-22 16:15:56 -0500

    only allow 6 indents

--- dist/index.js
+++ dist/index.js
@@ -4925,7 +4925,7 @@ function (_React$Component) {
         key: 0,
         type: "button",
         "data-title": language.controls.increaseIndent,
-        disabled: currentIndent >= 10,
+        disabled: currentIndent >= 6,
         className: "control-item button button-indent-increase".concat(currentIndent > 0 && currentIndent < 6 ? ' active' : ''),
         onClick: this.increaseIndent
       }, external_react_default.a.createElement("i", {
